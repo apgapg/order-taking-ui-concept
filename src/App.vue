9d3d5e84387f1a86afa3a7b362baa8385b433a01
Ayush P Gupta <ayushpguptaapg@gmail.com>
2022-02-18 08:53:42 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+
+</style>
